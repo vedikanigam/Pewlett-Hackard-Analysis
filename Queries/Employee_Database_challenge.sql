36adbc7b412117f5e6b0b09a44dbf1e5002cd9ca
Vedika Nigam <vedika.nigam@gmail.com>
2021-11-07 18:59:38 -0500

    Updated files

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -70,7 +70,7 @@ SELECT e.emp_no,
 		t.title,
 		t.from_date,
 		t.to_date
-INTO retirement_titles
+--INTO retirement_titles
 FROM employees as e
 INNER JOIN titles as t
 ON (e.emp_no = t.emp_no)
@@ -79,13 +79,13 @@ ORDER BY e.emp_no
 
 --Remove duplicate entries 
 SELECT DISTINCT ON (rt.emp_no) rt.emp_no, rt.first_name, rt.last_name, rt.title
-INTO unique_titles
+--INTO unique_titles
 FROM retirement_titles as rt
 ORDER BY emp_no, to_date DESC 
 
 --Count of retiring employees by title
 SELECT COUNT(ut.title), ut.title
-INTO retiring_titles
+--INTO retiring_titles
 FROM unique_titles as ut
 GROUP BY ut.title
 ORDER BY count DESC;
@@ -98,7 +98,7 @@ SELECT DISTINCT ON(e.emp_no) e.emp_no,
 		de.from_date,
 		de.to_date,
 		t.title
-INTO mentorship_eligibility
+--INTO mentorship_eligibility
 FROM employees as e
 JOIN dept_emp as de
 ON (e.emp_no = de.emp_no)
@@ -108,6 +108,11 @@ WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
 AND (de.to_date = '9999-01-01')
 ORDER BY emp_no;
 
-		
+
+SELECT COUNT(me.title), me.title
+--INTO retiring_titles
+FROM mentorship_eligibility as me
+GROUP BY me.title
+ORDER BY count DESC;		
 
 
